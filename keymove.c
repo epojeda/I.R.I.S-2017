f3391f7601fc62beb975c67e7e3ef38f5adff823
epojeda <epojeda@gmail.com>
2017-08-03 10:46:59 -0400

    recognizing change in speed with use of arrow keys to move and change direction.

--- keymove.c
+++ keymove.c
@@ -2,10 +2,10 @@
 #include <sys/time.h>
 #include <termios.h>
 #include <stdio.h>
-//#include <fstream>
-//#include <iostream>
 struct termios mNewTerm;
 struct termios mOrgTerm;
+int Lspeed = 0;
+int Rspeed = 0;
 void initKey()
 {
 	// Sets the terminal to get real-time keyboard values
@@ -37,75 +37,84 @@ char getKey()
 int forward()
 {
 	printf("Moving forward \n");
+	Lspeed = Lspeed+ 10;
+	Rspeed = Rspeed + 10;
+	printf("Left speed =" "%d \n", Lspeed);
+	printf("Right speed =" "%d \n", Rspeed);
 }
 
 int right()
 {
 	printf("Turning right \n");
+	Lspeed = Lspeed + 10;
+	Rspeed = Rspeed - 10;
+	printf("Left speed =" "%d \n", Lspeed);
+	printf("Right speed =" "%d \n", Rspeed);
 }
 
 int left()
 {
 	printf("Turning left \n");
+	Lspeed = Lspeed - 10;
+	Rspeed = Rspeed + 10;
+	printf("Left speed =" "%d \n", Lspeed);
+	printf("Right speed =" "%d \n", Rspeed);
 }
 
 int backward()
 {
 	printf("Moving backwards \n");
+	Lspeed = Lspeed - 10;
+	Rspeed = Rspeed - 10;
+	printf("Left speed =" "%d \n", Lspeed);
+	printf("Right speed =" "%d \n", Rspeed);
 }
 
 int stop()
 {
 	printf("STOP!!! \n");
+	Lspeed = 0;
+	Rspeed = 0;
+	printf("Left speed =" "%d \n", Lspeed);
+	printf("Right speed =" "%d \n", Rspeed);
 }
+
 int main()
 {
 	initKey();
 	char key;
-	int speed;
-	speed = 0;
-	int newspeed;
 	while (1)
 	{		
 		key = getKey();
-
 		if (key != 0)
 		{	
 			printf("%c ",key);
 			printf("%d \n",key);
 		}
-		if (key == 65)
+		switch (key)
 		{
-			forward();
-			speed = speed + 10;
-			printf("speed =" "%d \n", speed);
-			speed =speed;
-		}
-		if (key == 66)
-		{
-			speed = speed - 10;
-			backward();
-			printf("speed =" "%d \n", speed);
-			speed = speed;
-		}
-		if (key == 67)
-		{
-			right();
-		}
-		if (key == 68)
-		{
-			left();
-		}
-		if (key == 32)
-		{
-			speed = 0;
-			stop();
-			printf("speed =" "%d \n", speed);
+			case 'A': // forward arrow key
+				forward();
+				break;
+			case 'B':
+				backward();
+				break; 
+			case 'C':
+				right();
+				break;
+			case 'D':
+				left();
+				break; 
+			case 32:
+				stop();
+				break;
+			case 'x': //exit
+				resetKey();
+				return 0;
 		}
 	}
-	resetKey();
-	return 0;
 }
+
 
 
 
